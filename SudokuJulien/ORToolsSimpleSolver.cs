c700a4c5544e4887f4085fc8e5b83857774bf595
Julien Codjia Sicard <olijien@gmail.com>
2025-01-23 01:55:28 +0100

    Initialisation du solver OR-Tools

---
+++ SudokuJulien/ORToolsSimpleSolver.cs
@@ -0,0 +1,12 @@
+using System;
+using Sudoku.Shared;
+
+namespace SudokuJulien;
+
+public class ORToolsSimpleSolver : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        return s;
+    }
+}
