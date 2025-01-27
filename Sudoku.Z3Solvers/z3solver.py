201d853364dd4ae39803143e3c5bdf5c1e338d31
jsboigeESG <j.boige@intervenant-ggeedu.fr>
2025-01-27 15:52:42 +0100

    Solver Python z3 simple

--- Sudoku.Z3Solvers/z3solver.py
+++ Sudoku.Z3Solvers/z3solver.py
@@ -1,8 +1,9 @@
-from z3 import Solver, Int, And, Distinct
+from z3 import *
 from timeit import default_timer
 
 # Fonction pour résoudre un Sudoku avec Z3
 def solve_sudoku_with_z3(grid):
+
     # Déclarez une matrice 9x9 de variables entières (Z3 variables)
     cells = [[Int(f"cell_{i}_{j}") for j in range(9)] for i in range(9)]
 
@@ -41,26 +42,28 @@ def solve_sudoku_with_z3(grid):
                 solver.add(cells[i][j] == grid[i][j])
 
     # Tentez de résoudre
-    if solver.check() == 'sat':
+    if solver.check() == sat:
         model = solver.model()
         solved_grid = [[model[cells[i][j]].as_long() for j in range(9)] for i in range(9)]
         return solved_grid
     else:
+        print("Aucune solution trouvée.")
         return None
 
 
-# Définir la grille de Sudoku initiale
-instance = [
-    [0, 0, 0, 0, 9, 4, 0, 3, 0],
-    [0, 0, 0, 5, 1, 0, 0, 0, 7],
-    [0, 8, 9, 0, 0, 0, 0, 4, 0],
-    [0, 0, 0, 0, 0, 0, 2, 0, 8],
-    [0, 6, 0, 2, 0, 1, 0, 5, 0],
-    [1, 0, 2, 0, 0, 0, 0, 0, 0],
-    [0, 7, 0, 0, 0, 0, 5, 2, 0],
-    [9, 0, 0, 0, 6, 5, 0, 0, 0],
-    [0, 4, 0, 9, 7, 0, 0, 0, 0],
-]
+# Définir la grille de Sudoku initiale (passée depuis le csharp ou explicitement pour travailler en autonomie)
+if 'instance' not in locals():
+    instance = (
+        (0, 0, 0, 0, 9, 4, 0, 3, 0),
+        (0, 0, 0, 5, 1, 0, 0, 0, 7),
+        (0, 8, 9, 0, 0, 0, 0, 4, 0),
+        (0, 0, 0, 0, 0, 0, 2, 0, 8),
+        (0, 6, 0, 2, 0, 1, 0, 5, 0),
+        (1, 0, 2, 0, 0, 0, 0, 0, 0),
+        (0, 7, 0, 0, 0, 0, 5, 2, 0),
+        (9, 0, 0, 0, 6, 5, 0, 0, 0),
+        (0, 4, 0, 9, 7, 0, 0, 0, 0),
+    )
 
 # Solve the Sudoku puzzle
 start = default_timer()
@@ -68,10 +71,9 @@ result = solve_sudoku_with_z3(instance)
 execution = default_timer() - start
 
 # Affichez la solution ou un message d'échec
+
 if result:
-    print("Sudoku résolu avec succès avec Z3 Solver:")
+    # print("Sudoku résolu avec succès avec Z3 Solver:")
     for row in result:
-        print(row)
-else:
-    print("Aucune solution trouvée.")
-print("Le temps de résolution est de : ", execution * 1000, "ms")
+        # print(row)
+    # print("Le temps de résolution est de : ", execution * 1000, "ms")
