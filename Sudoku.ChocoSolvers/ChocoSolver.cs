fcb764e4c6b67ebfef09cd6566c6ba562217c039
JEAN <jeansaddy96@gmail.com>
2025-01-28 17:08:29 +0100

    initialisation2

---
+++ Sudoku.ChocoSolvers/ChocoSolver.cs
@@ -0,0 +1,11 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.ChocoSolvers;
+
+public class ChocoSolver : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        return s;
+    }
+}
