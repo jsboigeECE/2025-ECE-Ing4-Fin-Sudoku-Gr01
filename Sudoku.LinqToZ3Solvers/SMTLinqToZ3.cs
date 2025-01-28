d3c0ca506a8787cc313e410e7ffa3d0f87510500
CADOZ <octave.cadoz@edu.ece.fr>
2025-01-28 14:30:23 +0100

    Résolution SMT avec LinqToZ3

---
+++ Sudoku.LinqToZ3Solvers/SMTLinqToZ3.cs
@@ -0,0 +1,12 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.LinqToZ3Solvers;
+
+public class SMTLinqToZ3 : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        return s;
+    }
+}
+
