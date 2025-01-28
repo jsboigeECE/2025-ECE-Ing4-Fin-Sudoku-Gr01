40af968e42040fff2d68c199822c10a26388af3b
Gabriel <gabrieldeolival@gmail.com>
2025-01-28 14:47:56 +0100

    Initialisation du solver techniques humaines vide

---
+++ Sudoku.GabrielOscar/HumanTechsEz.cs
@@ -0,0 +1,11 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.GabrielOscar;
+
+public class HumanTechsSimple : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        return s;
+    }
+}
