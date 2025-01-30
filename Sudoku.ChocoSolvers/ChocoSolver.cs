6117ae6f945e3ec9f6b3b6c14daa65590d84c428
jsboigeESG <j.boige@intervenant-ggeedu.fr>
2025-01-30 13:22:20 +0100

    Amorce choco solver

--- Sudoku.ChocoSolvers/ChocoSolver.cs
+++ Sudoku.ChocoSolvers/ChocoSolver.cs
@@ -1,8 +1,10 @@
 ﻿using Sudoku.Shared;
 using System;
+using org.chocosolver.solver.variables;
 
 namespace Sudoku.ChocoSolvers;
 
+// Je garde votre classe initiale, mais vous serez amenés à encréer plusieurs, typiquement héritant d'une classe de base et changeant les nombreux paramètres disponibles
 public class ChocoSolver : ISudokuSolver
 {
     public SudokuGrid Solve(SudokuGrid s)
@@ -10,13 +12,28 @@ public class ChocoSolver : ISudokuSolver
       
         
         // Résoudre avec Choco Solver
-         ResoudreSudoku(s.Cells);
+         ResoudreSudokuBacktracking(s.Cells);
          return s;
 
     }
 
-   
-    private bool ResoudreSudoku(int[,] grid)
+
+    private void ResoudreSudokuChocoSolver(SudokuGrid s)
+    {
+        // Je fais juste l'amorce pour vous montrer où vous pouvez trouver le code de Choco. Le reste sera dans la doc
+	    var model = new org.chocosolver.solver.Model();
+	    var cellVariables = model.intVarMatrix(9, 9, 1, 9);
+		// cf la doc pour la suite:
+		// https://choco-solver.org/docs/modeling/intconstraints/
+		// cf par exemple https://choco-solver.org/docs/solving/strategies/
+		// https://choco-solver.org/docs/advanced-usages/strategies/
+		//https://choco-solver.org/docs/solving/lns/
+    }
+
+
+
+	// Je laisse votre ancien code que j'ai juste simplifié, mais vous pourrez l'enlever
+	private bool ResoudreSudokuBacktracking(int[,] grid)
     {
         for (int row = 0; row < 9; row++)
         {
@@ -29,7 +46,7 @@ public class ChocoSolver : ISudokuSolver
                         if (EstValide(grid, row, col, num))
                         {
                             grid[row, col] = num;
-                            if (ResoudreSudoku(grid))
+                            if (ResoudreSudokuBacktracking(grid))
                                 return true;
                             grid[row, col] = 0;
                         }
