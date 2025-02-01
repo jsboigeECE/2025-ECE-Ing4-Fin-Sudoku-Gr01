f9f71e12f7be06a3dc2cc1c5c5db26370a92b64d
Pual0 <paul.paulab@gmail.com>
2025-02-01 18:56:08 +0100

    Re Init Solver Yices Vide

---
+++ Sudoku.FaresPaulSolvers/FPYicesSimpleSolver.cs
@@ -0,0 +1,11 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.FaresPaulSolvers;
+
+public class FPYicesSimpleSolver : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        return s;
+    }
+}
